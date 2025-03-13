e7cc45de2ae733f2732eb08db974883ed0f1a284
Graven | Developpement <gravenilvec@gmail.com>
2025-03-13 09:29:23 +0100

    all*

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ Route::get('/', function () {
 });
 
 Route::get('/articles', function () {
+    $articles = ["Article 1", "Article 2", "Article 3", "Article 4"];
     $nbArticles = 3;
     $context = [
         'nbArticles' => $nbArticles, 
